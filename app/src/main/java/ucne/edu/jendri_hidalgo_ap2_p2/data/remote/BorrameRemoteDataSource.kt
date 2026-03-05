990c5587dd6ed1af5c693f97bd3586a9d940498c
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2026-03-04 20:11:15 -0400

    Agregue los usecase.

---
+++ app/src/main/java/ucne/edu/jendri_hidalgo_ap2_p2/data/remote/BorrameRemoteDataSource.kt
@@ -0,0 +1,9 @@
+package ucne.edu.jendri_hidalgo_ap2_p2.data.remote
+
+import javax.inject.Inject
+
+class BorrameRemoteDataSource @Inject constructor(
+
+) {
+
+}
---
+++ app/src/main/java/ucne/edu/jendri_hidalgo_ap2_p2/data/repository/BorrameRepositoryImpl.kt
@@ -0,0 +1,9 @@
+package ucne.edu.jendri_hidalgo_ap2_p2.data.repository
+
+import javax.inject.Inject
+
+class BorrameRepositoryImpl @Inject constructor(
+
+) {
+
+}
