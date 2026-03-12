dc95b46265a8f31ac57e2a8f032dd209b1af7b5d
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2026-03-11 23:39:38 -0400

    Modifique la interfaz del repositorio.

--- app/src/main/java/ucne/edu/jendri_hidalgo_ap2_p2/domain/repository/JugadorRepository.kt
+++ app/src/main/java/ucne/edu/jendri_hidalgo_ap2_p2/domain/repository/JugadorRepository.kt
@@ -1,14 +1,13 @@
 package ucne.edu.jendri_hidalgo_ap2_p2.domain.repository
 
+import kotlinx.coroutines.flow.Flow
 import ucne.edu.jendri_hidalgo_ap2_p2.data.remote.Resource
 import ucne.edu.jendri_hidalgo_ap2_p2.domain.model.Jugador
 
-
 interface JugadorRepository {
-    suspend fun getJugadores(
-        nombres: String?,
-        email: String?
-    ): Resource<List<Jugador>>
-
-    suspend fun getJugadorDetail(id: Int): Resource<Jugador>
+    fun getJugadores(nombres: String?, email: String?): Flow<Resource<List<Jugador>>>
+    fun getJugadorDetail(id: Int): Flow<Resource<Jugador>>
+    fun createJugador(nombres: String, email: String): Flow<Resource<Unit>>
+    fun updateJugador(id: Int, nombres: String, email: String): Flow<Resource<Unit>>
+    fun saveJugador(id: Int?, nombres: String, email: String): Flow<Resource<Unit>>
 }
