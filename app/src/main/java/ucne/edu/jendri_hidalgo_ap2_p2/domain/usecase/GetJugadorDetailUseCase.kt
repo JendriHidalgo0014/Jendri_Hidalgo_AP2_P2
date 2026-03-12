b0d118812404af1bd8bcbfe54ba0b94dd1cd3ff7
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2026-03-11 23:38:21 -0400

    Modifique GetJugadorDetailUseCase.

--- app/src/main/java/ucne/edu/jendri_hidalgo_ap2_p2/domain/usecase/GetJugadorDetailUseCase.kt
+++ app/src/main/java/ucne/edu/jendri_hidalgo_ap2_p2/domain/usecase/GetJugadorDetailUseCase.kt
@@ -1,10 +1,13 @@
 package ucne.edu.jendri_hidalgo_ap2_p2.domain.usecase
 
+import kotlinx.coroutines.flow.Flow
+import ucne.edu.jendri_hidalgo_ap2_p2.data.remote.Resource
+import ucne.edu.jendri_hidalgo_ap2_p2.domain.model.Jugador
 import ucne.edu.jendri_hidalgo_ap2_p2.domain.repository.JugadorRepository
 import javax.inject.Inject
 
 class GetJugadorDetailUseCase @Inject constructor(
     private val repository: JugadorRepository
 ) {
-    suspend operator fun invoke(id: Int) = repository.getJugadorDetail(id)
+    operator fun invoke(id: Int): Flow<Resource<Jugador>> = repository.getJugadorDetail(id)
 }
