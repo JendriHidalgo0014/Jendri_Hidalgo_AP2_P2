24175fcbaf4b523b492b7156bf394e9db46b6637
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2026-03-11 23:41:52 -0400

    Agregue SaveJugadorUseCase.

---
+++ app/src/main/java/ucne/edu/jendri_hidalgo_ap2_p2/domain/usecase/SaveJugadorUseCase.kt
@@ -0,0 +1,16 @@
+package ucne.edu.jendri_hidalgo_ap2_p2.domain.usecase
+
+import kotlinx.coroutines.flow.Flow
+import ucne.edu.jendri_hidalgo_ap2_p2.data.remote.Resource
+import ucne.edu.jendri_hidalgo_ap2_p2.domain.repository.JugadorRepository
+import javax.inject.Inject
+
+class SaveJugadorUseCase @Inject constructor(
+    private val repository: JugadorRepository
+) {
+    operator fun invoke(
+        id: Int?,
+        nombres: String,
+        email: String
+    ): Flow<Resource<Unit>> = repository.saveJugador(id, nombres, email)
+}
