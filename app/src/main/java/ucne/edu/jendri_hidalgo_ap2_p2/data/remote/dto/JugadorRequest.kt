bf97dde6dc8a89c19c1c570bb31e67a06fc61f37
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2026-03-11 21:09:21 -0400

    Agregue el request y response de jugador.

---
+++ app/src/main/java/ucne/edu/jendri_hidalgo_ap2_p2/data/remote/dto/JugadorRequest.kt
@@ -0,0 +1,6 @@
+package ucne.edu.jendri_hidalgo_ap2_p2.data.remote.dto
+
+data class JugadorRequest(
+    val nombres: String,
+    val email: String,
+)
---
+++ app/src/main/java/ucne/edu/jendri_hidalgo_ap2_p2/data/remote/dto/JugadorResponse.kt
@@ -0,0 +1,7 @@
+package ucne.edu.jendri_hidalgo_ap2_p2.data.remote.dto
+
+data class JugadorResponse(
+    val jugadorId: Int,
+    val nombres: String,
+    val email: String,
+)
