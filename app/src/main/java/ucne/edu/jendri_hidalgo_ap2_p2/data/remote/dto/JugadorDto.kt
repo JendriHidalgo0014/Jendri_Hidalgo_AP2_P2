6ce22a035c006633ed80897dcc9957b54a9b0659
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2026-03-11 23:38:51 -0400

    Modifique JugadorDto.

--- app/src/main/java/ucne/edu/jendri_hidalgo_ap2_p2/data/remote/dto/JugadorDto.kt
+++ app/src/main/java/ucne/edu/jendri_hidalgo_ap2_p2/data/remote/dto/JugadorDto.kt
@@ -2,15 +2,13 @@ package ucne.edu.jendri_hidalgo_ap2_p2.data.remote.dto
 
 import ucne.edu.jendri_hidalgo_ap2_p2.domain.model.Jugador
 
-data class JugadorResponseDto(
-    val items: List<JugadorDto>
-)
 data class JugadorDto(
-    val id: Int,
+    val jugadorId: Int,
     val nombres: String,
     val email: String,
 ) {
-    fun toDomain() = Jugador(
-        id, nombres, email,
-    )
+    fun toDomain() = Jugador(jugadorId, nombres, email)
 }
+
+
+
