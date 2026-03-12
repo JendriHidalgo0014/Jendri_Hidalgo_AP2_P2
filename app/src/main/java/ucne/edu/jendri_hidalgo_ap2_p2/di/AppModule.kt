3725445d52f92030070582c918f15d21279881b7
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2026-03-11 21:07:05 -0400

    Agregue el AppModule.

--- app/src/main/java/ucne/edu/jendri_hidalgo_ap2_p2/di/AppModule.kt
+++ app/src/main/java/ucne/edu/jendri_hidalgo_ap2_p2/di/AppModule.kt
@@ -6,11 +6,28 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import ucne.edu.jendri_hidalgo_ap2_p2.data.remote.JugadorRemoteDataSource
+import ucne.edu.jendri_hidalgo_ap2_p2.data.remote.JugadoresApi
+import ucne.edu.jendri_hidalgo_ap2_p2.data.repository.JugadorRepositoryImpl
+import ucne.edu.jendri_hidalgo_ap2_p2.domain.repository.JugadorRepository
 import javax.inject.Singleton
+import kotlin.jvm.java
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideApi(moshi: Moshi): JugadoresApi {
+        return Retrofit.Builder()
+            .baseUrl("https://gestionhuacalesapi.azurewebsites.net/swagger/index.html")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(JugadoresApi::class.java)
+    }
 
     @Singleton
     @Provides
@@ -21,4 +38,9 @@ object AppModule {
             .build()
     }
 
+    @Provides
+    @Singleton
+    fun provideJugadorRepository(remoteDataSource: JugadorRemoteDataSource): JugadorRepository {
+        return JugadorRepositoryImpl(remoteDataSource)
+    }
 }
