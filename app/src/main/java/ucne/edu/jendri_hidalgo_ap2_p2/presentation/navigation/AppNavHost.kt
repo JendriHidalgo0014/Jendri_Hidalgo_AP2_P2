614394d825a668502b72f4f4e7995376f1d413a5
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2026-03-11 23:37:22 -0400

    Modifique la navegacion.

--- app/src/main/java/ucne/edu/jendri_hidalgo_ap2_p2/presentation/navigation/AppNavHost.kt
+++ app/src/main/java/ucne/edu/jendri_hidalgo_ap2_p2/presentation/navigation/AppNavHost.kt
@@ -4,30 +4,41 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
 import ucne.edu.jendri_hidalgo_ap2_p2.presentation.tareas.details.DetailJugadorScreen
+import ucne.edu.jendri_hidalgo_ap2_p2.presentation.tareas.edit.EditJugadorScreen
 import ucne.edu.jendri_hidalgo_ap2_p2.presentation.tareas.list.ListJugadorScreen
 
 @Composable
-fun AppNavHost(
-    navHostController: NavHostController
-) {
+fun AppNavHost(navHostController: NavHostController) {
     NavHost(
         navController = navHostController,
         startDestination = Screen.JugadorList
     ) {
         composable<Screen.JugadorList> {
             ListJugadorScreen(
-                onJugadorClick = { Id ->
-                    navHostController.navigate(Screen.JugadorDetail(Id))
+                onJugadorClick = { id ->
+                    navHostController.navigate(Screen.JugadorDetail(id))
+                },
+                onAddClick = {
+                    navHostController.navigate(Screen.JugadorEdit(0))
                 }
             )
         }
 
-        composable<Screen.JugadorDetail> {
+        composable<Screen.JugadorDetail> { backStackEntry ->
+            val args = backStackEntry.toRoute<Screen.JugadorDetail>()
             DetailJugadorScreen(
-                onBack = {
-                    navHostController.navigateUp()
-                }
+                onBack = { navHostController.navigateUp() },
+                onEdit = { navHostController.navigate(Screen.JugadorEdit(args.id)) }
+            )
+        }
+
+        composable<Screen.JugadorEdit> { backStackEntry ->
+            val args = backStackEntry.toRoute<Screen.JugadorEdit>()
+            EditJugadorScreen(
+                jugadorId = args.id,
+                goBack = { navHostController.navigateUp() }
             )
         }
     }
